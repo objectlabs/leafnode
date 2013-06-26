9eff962174b0dd82917db90ff7909d126d59df80
Jared D. Cottrell <jared@objectlabs.com>
2013-06-25 19:29:57 -0700

    Upgrading to MongoClient in leafnode.connect(). Also fixing some bugs using fibers.

--- test/all.js
+++ test/all.js
@@ -15,7 +15,7 @@
  *  limitations under the License.
  */
 
-require('fibers');
+var Fiber = require('fibers');
 
 /****************************************************************************************************
  * run
